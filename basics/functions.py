ead3e001d13437ac5160ba4fa71afdf88732e93b
Max Sun <130513985+maxsunc@users.noreply.github.com>
2025-08-30 16:15:12 -0400

    basics done

---
+++ basics/functions.py
@@ -0,0 +1,31 @@
+
+# example
+def add(a, b):
+    return a + b
+
+#example 2 (void = no return (None))
+def greet(name):
+    print(f"Hello, {name}")
+
+#example 3 default parameters
+def greet(name="Guest"):
+    print(f"Hello, {name}")
+
+#example 4 tuple as parameter
+def sum_all(*numbers):
+    total = 0
+    for number in numbers:
+        total += number
+    return total
+
+# using functions (only after defining them)
+print(add(2, 3))
+
+greet("Alice")
+greet() # uses default parameter
+
+print(sum_all(1, 2, 3, 4, 5))
+
+#using functions with prefixed
+print(add(a=5, b=7))
+
