524e2ad84a514ab2fc5721dec2daa105ea3160a0
Max Sun <130513985+maxsunc@users.noreply.github.com>
2025-09-08 13:42:23 -0400

    updates

--- datastructures.py
+++ datastructures.py
@@ -126,3 +126,20 @@ def find_sum(root):
 
 print("trees ex:")
 print("summ of tree is : " + str(find_sum(root)))
+
+
+# graph (directed):
+class GraphNode:
+    def __init__(self, val):
+        self.val = val
+        self.neighbors = []  # List of GraphNode
+
+# Example usage
+nodeA = GraphNode('A')
+nodeB = GraphNode('B')
+nodeC = GraphNode('C')
+nodeA.neighbors.append(nodeB)
+nodeA.neighbors.append(nodeC)
+nodeB.neighbors.append(nodeC)
+print("graph ex:")
+print("node A neighbors: " + str([n.val for n in nodeA.neighbors]))  # Output: ['B', 'C']
--- playground.py
+++ playground.py
@@ -1,37 +1,16 @@
-class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        seen = {}  # <str, int>
-        maxLen = 1
-        curLen = 1
-        i = 0
-        seen[s[i]] = 1
-        curLeeway = k
-        while i < len(s):
-            repeatingChar = s[i]  # = C
-            # go until we run into a problem
-            right = i + curLen
-            while right < len(s):
-                print("i: " + str(i) + " right: " + str(right) + " curLen: " + str(curLen) + " curLeeway: " + str(curLeeway))
-                if repeatingChar != s[right]:
-                    curLeeway -= 1
-                curLen += 1
-                right += 1
-                
-                if curLeeway < 0:
-                    curLen -= 1
-                    i += 1
-                    maxLen = max(maxLen, curLen)
-                    break
-                else:
-                    maxLen = max(maxLen, curLen)
-            # This next line was improperly indented - it should be part of the outer while loop
-            if right == len(s):
-                break
-        return maxLen
 
+# graph (directed):
+class GraphNode:
+    def __init__(self, val):
+        self.val = val
+        self.neighbors = []  # List of GraphNode
 
-# test cases:
-# print(Solution().characterReplacement("ABAB", 2)) # 4
-print(Solution().characterReplacement("AAABABB", 1)) # 5
-assert Solution().characterReplacement("ABAB", 2) == 4
-assert Solution().characterReplacement("AAABABB", 1) == 5
+# Example usage
+nodeA = GraphNode('A')
+nodeB = GraphNode('B')
+nodeC = GraphNode('C')
+nodeA.neighbors.append(nodeB)
+nodeA.neighbors.append(nodeC)
+nodeB.neighbors.append(nodeC)
+print("graph ex:")
+print("node A neighbors: " + str([n.val for n in nodeA.neighbors]))  # Output: ['B', 'C']
